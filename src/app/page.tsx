ce93fdfc976c33369012fde2a535842b8d18b6f1
YashKVR <yashincrypto5@gmail.com>
2024-10-17 11:28:18 +0530

    added password authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,10 @@ export default function Home() {
   return (
     <div>
       Logged In!
-      <Button onClick={() => signOut()}>
+      <Button onClick={async() => {
+        await signOut();
+        window.location.reload();
+      }}>
         Sign Out
       </Button>
     </div>
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
+import {TriangleAlert} from "lucide-react";
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
@@ -22,7 +23,22 @@ export const SignInCard = ({setState}:SignInCardProps) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const [pending, setPending] = useState(false);
+
+    const onPasswordSignIn = (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setPending(true);
+
+        signIn("password", {email, password, flow: "signIn"})
+        .catch(() => {
+            setError("Invalid Email or Password");
+        })
+        .finally(() => {
+            setPending(false);
+            window.location.reload();
+        });
+    }
 
     const onProviderSignIn = (value: "google") =>{
         setPending(true);
@@ -42,8 +58,12 @@ export const SignInCard = ({setState}:SignInCardProps) => {
                 Use Your Email or another service to continue
             </CardDescription>
             </CardHeader>
+            {!!error && (<div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+            <TriangleAlert className="size-4"/>
+            <p>{error}</p>
+            </div> )}
             <CardContent className="space-y-5 px-0 pb-0">
-                <form className="space-y-2.5">
+                <form onSubmit={onPasswordSignIn} className="space-y-2.5">
                     <Input 
                     disabled={pending}
                     value={email}
