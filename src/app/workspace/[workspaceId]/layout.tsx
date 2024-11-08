b8825e9c9f91944f5f9d837cd08c3422b91b0720
YashKVR <yashincrypto5@gmail.com>
2024-11-08 11:24:27 +0530

    added workspace members

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -3,18 +3,43 @@ import React from "react"
 import { Toolbar } from "./toolbar";
 import { Sidebar } from "./sidebar";
 
-interface WorkspaceIdLayoutProps{
-    children: React.ReactNode;
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import { WorkspaceSidebar } from "./workspace-sidebar";
+
+interface WorkspaceIdLayoutProps {
+  children: React.ReactNode;
 }
 
-const workspaceIdLayout = ({children}: WorkspaceIdLayoutProps) => {
+const workspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
   return (
     <div className="h-full">
-        <Toolbar />
-        <div className="flex h-[calc(100vh-40px)]">
-          <Sidebar />
-        {children}
-        </div>
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        <ResizablePanelGroup
+         direction="horizontal"
+         autoSave="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5e2c5f]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+
+          <ResizableHandle withHandle/>
+
+          <ResizablePanel minSize={20}>
+            {children}
+          </ResizablePanel>
+        </ResizablePanelGroup>
+
+      </div>
     </div>
   )
 }
