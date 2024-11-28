84f5e0f3e2535c5dfcb765aad1f4a7d88c1e1b3c
YashKVR <yashincrypto5@gmail.com>
2024-11-28 12:07:18 +0530

    Add Edit and Delete message functionality

--- convex/messages.ts
+++ convex/messages.ts
@@ -12,32 +12,32 @@ const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => {
     )
     .collect();
 
-    if(messages.length === 0) {
-        return {
-            count: 0,
-            image: undefined,
-            timestamp:0
-        }
-    }
-
-    const lastMessage = messages[messages.length - 1];
-    const lastMessageMember = await populateMember(ctx, lastMessage.memberId);
-
-    if(!lastMessageMember) {
-        return {
-            count: 0,
-            image: undefined,
-            timestamp:0
-        }
-    }
-
-    const lastMessageUser = await populateUser(ctx, lastMessageMember.userId);
-
+  if (messages.length === 0) {
     return {
-        count: messages.length,
-        image: lastMessageUser?.image,
-        timestamp: lastMessage._creationTime
-    }
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessage = messages[messages.length - 1];
+  const lastMessageMember = await populateMember(ctx, lastMessage.memberId);
+
+  if (!lastMessageMember) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessageUser = await populateUser(ctx, lastMessageMember.userId);
+
+  return {
+    count: messages.length,
+    image: lastMessageUser?.image,
+    timestamp: lastMessage._creationTime,
+  };
 };
 
 const populateReactions = async (ctx: QueryCtx, messageId: Id<"messages">) => {
@@ -68,93 +68,156 @@ const getMember = async (
     .unique();
 };
 
+export const remove = mutation({
+  args: {
+    id: v.id("messages"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const message = await ctx.db.get(args.id);
+
+    if(!message){
+      throw new Error("Message not found");
+    }
+
+    const member = await getMember(ctx, message.workspaceId, userId);
+
+    if(!member || member._id !== message.memberId){
+      throw new Error("Unauthorized");
+    }
+    
+    await ctx.db.delete(args.id);
+
+    return args.id;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("messages"),
+    body: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const message = await ctx.db.get(args.id);
+
+    if(!message){
+      throw new Error("Message not found");
+    }
+
+    const member = await getMember(ctx, message.workspaceId, userId);
+
+    if(!member || member._id !== message.memberId){
+      throw new Error("Unauthorized");
+    }
+    
+    await ctx.db.patch(args.id, {
+      body: args.body,
+      updatedAt: Date.now(),
+    });
+
+    return args.id;
+  },
+});
+
 export const get = query({
-    args:{
-        channelId: v.optional(v.id("channels")),
-        conversationId: v.optional(v.id("conversations")),
-        parentMessageId: v.optional(v.id("messages")),
-        paginationOpts: paginationOptsValidator,
-    },
-    handler: async(ctx,args) =>{
-        const userId = await getAuthUserId(ctx);
+  args: {
+    channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
+    parentMessageId: v.optional(v.id("messages")),
+    paginationOpts: paginationOptsValidator,
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
 
-        if (!userId) {
-        throw new Error("Unauthorized");
-        }
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
 
-        let _conversationId = args.conversationId;
+    let _conversationId = args.conversationId;
 
-        //Only possible if we are replying in a thread in a one on one conversation
+    //Only possible if we are replying in a thread in a one on one conversation
     if (!args.conversationId && !args.channelId && args.parentMessageId) {
-        const parentMessage = await ctx.db.get(args.parentMessageId);
-  
-        if (!parentMessage) {
-          throw new Error("Parent message not found");
-        }
-  
-        _conversationId = parentMessage.conversationId;
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+
+      if (!parentMessage) {
+        throw new Error("Parent message not found");
       }
 
-      const results = await ctx.db
+      _conversationId = parentMessage.conversationId;
+    }
+
+    const results = await ctx.db
       .query("messages")
-      .withIndex("by_channel_id_parent_message_id_conversation_id", (q) => q
-      .eq("channelId", args.channelId)
-      .eq("parentMessageId", args.parentMessageId)
-      .eq("conversationId", _conversationId)
-    )
-    .order("desc")
-    .paginate(args.paginationOpts);
+      .withIndex("by_channel_id_parent_message_id_conversation_id", (q) =>
+        q
+          .eq("channelId", args.channelId)
+          .eq("parentMessageId", args.parentMessageId)
+          .eq("conversationId", _conversationId)
+      )
+      .order("desc")
+      .paginate(args.paginationOpts);
 
     return {
       ...results,
       page: (
         await Promise.all(
-          results.page.map(async(message) => {
+          results.page.map(async (message) => {
             const member = await populateMember(ctx, message.memberId);
             const user = member ? await populateUser(ctx, member.userId) : null;
 
-            if(!member || !user){
+            if (!member || !user) {
               return null;
             }
 
             const reactions = await populateReactions(ctx, message._id);
             const thread = await populateThread(ctx, message._id);
             const image = message.image
-            ? await ctx.storage.getUrl(message.image)
-            : undefined;
+              ? await ctx.storage.getUrl(message.image)
+              : undefined;
 
             const reactionsWithCounts = reactions.map((reaction) => {
               return {
                 ...reaction,
-                count: reactions.filter((r) => r.value === reaction.value).length
-              }
-            })
+                count: reactions.filter((r) => r.value === reaction.value)
+                  .length,
+              };
+            });
 
             const dedupedreactions = reactionsWithCounts.reduce(
-              (acc, reaction) =>{
+              (acc, reaction) => {
                 const existingReaction = acc.find(
-                  (r) => r.value === reaction.value,
-                )
+                  (r) => r.value === reaction.value
+                );
 
-                if(existingReaction){
+                if (existingReaction) {
                   existingReaction.memberIds = Array.from(
                     new Set([...existingReaction.memberIds, reaction.memberId])
-                  )
-                }
-                else{
-                  acc.push({...reaction, memberIds:[reaction.memberId]})
+                  );
+                } else {
+                  acc.push({ ...reaction, memberIds: [reaction.memberId] });
                 }
 
-                return acc
+                return acc;
               },
               [] as (Doc<"reactions"> & {
                 count: number;
                 memberIds: Id<"members">[];
               })[]
-            )
+            );
 
             const reactionsWithoutMemberIdProperty = dedupedreactions.map(
-              ({memberId, ...rest}) => rest
+              ({ memberId, ...rest }) => rest
             );
 
             return {
@@ -165,16 +228,16 @@ export const get = query({
               reactions: reactionsWithoutMemberIdProperty,
               threadCount: thread.count,
               threadImage: thread.image,
-              threadTimestamp: thread.timestamp
-            }
+              threadTimestamp: thread.timestamp,
+            };
           })
         )
       ).filter(
         (message): message is NonNullable<typeof message> => message !== null
-      )
+      ),
     };
-    }
-})
+  },
+});
 
 export const create = mutation({
   args: {
@@ -218,7 +281,7 @@ export const create = mutation({
       channelId: args.channelId,
       conversationId: _conversationId,
       workspaceId: args.workspaceId,
-      parentMessageId: args.parentMessageId
+      parentMessageId: args.parentMessageId,
     });
 
     return messageId;
--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -36,7 +36,7 @@ export const Toolbar = ({
 
         {!hideThreadButton && (
           <Hint label="Reply To Message">
-            <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <Button variant="ghost" size="iconSm" disabled={isPending} onClick={handleThread}>
               <MessageSquareTextIcon className="size-4" />
             </Button>
           </Hint>
@@ -44,7 +44,7 @@ export const Toolbar = ({
 
         {isAuthor && (
         <Hint label="Edit Message">
-          <Button variant="ghost" size="iconSm" disabled={isPending}>
+          <Button variant="ghost" size="iconSm" disabled={isPending} onClick={handleEdit}>
             <Pencil className="size-4" />
           </Button>
         </Hint>
@@ -52,7 +52,7 @@ export const Toolbar = ({
 
         {isAuthor && (
         <Hint label="Delete Message">
-          <Button variant="ghost" size="iconSm" disabled={isPending}>
+          <Button variant="ghost" size="iconSm" disabled={isPending} onClick={handleDelete}>
             <Trash className="size-4" />
           </Button>
         </Hint>
