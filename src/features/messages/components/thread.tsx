5f2c672db7761c8ffc11079e122ad5d3e8729493
yashkvr <yash@coincade.io>
2024-12-09 14:28:12 +0530

    Added Message Thread Bar

--- src/features/messages/components/thread.tsx
+++ src/features/messages/components/thread.tsx
@@ -219,6 +219,7 @@ export const Thread = ({ messageId, onClose }: ThreadProps) => {
                   hideThreadButton
                   threadCount={message.threadCount}
                   threadImage={message.threadImage}
+                  threadName={message.threadName}
                   threadTimestamp={message.threadTimestamp}
                 />
               );
