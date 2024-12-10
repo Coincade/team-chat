a8fe7197c3a801e9ccf566faa658d6e6ea3575d1
yashkvr <yash@coincade.io>
2024-12-10 15:01:57 +0530

    Added Default workspace for New User

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,51 @@ import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 import { useRouter } from "next/navigation";
 import { Loader } from "lucide-react";
+import { useJoin } from "@/features/workspaces/api/use-join";
+import { Id } from "../../convex/_generated/dataModel";
+import { toast } from "sonner";
 
 export default function Home() {
+  const default_workspace_id = process.env.NEXT_PUBLIC_DEFAULT_WORKSPACE_ID;
+  const default_workspace_joincode = process.env.NEXT_PUBLIC_DEFAULT_WORKSPACE_JOINCODE;
+
   const router = useRouter();
   const [open, setOpen] = useCreateWorkspaceModal();
 
   const {data, isLoading} = useGetWorkspaces();
+  const { mutate, isPending } = useJoin();
 
   const workspaceId = useMemo(()=>data?.[0]?._id,[data])
 
+  const handleJoin = (workspaceId:Id<"workspaces">, value: string) => {
+    mutate(
+      { workspaceId, joinCode: value },
+      {
+        onSuccess: (id) => {
+          router.replace(`/workspace/${id}`);
+          toast.success("Workspace Joined");
+        },
+        onError:() => {
+          toast.error("Failed to join workspace")
+        }
+      }
+    );
+  };
+
 
   useEffect(()=>{
-    if(isLoading) return;
+    if(isLoading || isPending) return;
 
     if(workspaceId){
       router.replace(`/workspace/${workspaceId}`)
-    }else if(!open){
-      setOpen(true);
-      
     }
-  },[workspaceId, isLoading,open, setOpen, router]);
+    // else if(!open){
+    //   setOpen(true);      
+    // }
+    else{
+      handleJoin(default_workspace_id as Id<"workspaces">, default_workspace_joincode as string);
+    }
+  },[workspaceId, isLoading, isPending, open, setOpen, router]);
 
 
   return (
