978d542821055bda534311dccce973140c602f70
YashKVR <yashincrypto5@gmail.com>
2025-02-10 11:11:29 +0530

    Add fix for image render in quill renderer

--- src/components/renderer.tsx
+++ src/components/renderer.tsx
@@ -21,7 +21,7 @@ const Renderer = ({value}: RendererProps) =>{
                 toolbar: false,
                 clipboard: false,
             },
-            formats: ['bold', 'italic', 'link', 'list']
+            formats: ['bold', 'italic', 'link', 'list', 'image']
         });
 
 
